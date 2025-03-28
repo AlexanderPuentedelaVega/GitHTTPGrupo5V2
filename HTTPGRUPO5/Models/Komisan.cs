f15389a0fbae7e09f7c080fa7e2ecbdc3ea4c4ba
Tony.L <rafael.lopez.r@tecsup.edu.pe>
2025-03-28 18:20:14 -0500

    Create zzzzzzz.cs

---
+++ HTTPGRUPO5/Models/Komisan.cs
@@ -0,0 +1,11 @@
+namespace HTTPGRUPO5.Models;
+
+public class Komisan
+{
+    public int Id { get; set; }
+    public string Nombre { get; set; }
+    public string Apellido { get; set; }
+    public string DNI { get; set; }
+    public string Correo { get; set; }
+    public string Telefono { get; set; }
+}
